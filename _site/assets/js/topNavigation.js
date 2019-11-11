d98b220c51a7140c1b55ea96d926ae2477efdaa1
Marko Bjelonic <marko-bjelonic@t-online.de>
2019-11-11 11:10:38 +0100

    Reduce white space in js.

--- _site/assets/js/topNavigation.js
+++ _site/assets/js/topNavigation.js
@@ -1,8 +1,8 @@
 function topNavigation() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
