b04c6c9fcca0b466c846eb45d3ad97505afae1e4
Marko Bjelonic <marko-bjelonic@t-online.de>
2019-11-11 10:24:51 +0100

    Moved skripts to end.

---
+++ assets/js/topNavigation.js
@@ -0,0 +1,8 @@
+function topNavigation() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
